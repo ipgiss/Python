cd57854d1ce97bceaf6d4b46e968afac4547555d
ipgiss <104978667+ipgiss@users.noreply.github.com>
2022-08-03 10:00:07 +0300

    add shorter version

--- 12_lucky_ticket.py
+++ 12_lucky_ticket.py
@@ -29,3 +29,12 @@ Sample Input 2:
 Sample Output 2:
 Обычный
 '''
+
+'''
+# shorter version:
+s = input()
+if int(s[0]) + int(s[1]) + int(s[2]) ==  int(s[3]) + int(s[4]) + int(s[5]):
+    print('Счастливый')
+else:
+    print('Обычный')
+'''
